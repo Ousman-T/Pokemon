7c7978b84124c6174283dbf581ee9b9ad54d089c
Ousman Touray <otouray14@gmail.com>
2023-04-18 17:24:05 -0400

    connected to DB

---
+++ models/Pokemons.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const pokemonSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: false
+    }
+})
+
+
+const Pokemon = mongoose.model('Pokemon', pokemonSchema);
+module.exports = pokemonSchema;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
 const pokemon = require('./models/pokemon');
 const React = require('react');
+const Pokemon = require('./models/Pokemons');
+const connectToDB = require('./config/db');
 // const bodyparser = require('bodyparser');
 
 //=============Configuring Engine
@@ -30,18 +33,22 @@ app.get('/', (req, res) => {
 // Index Route
 app.get('/pokemon', (req, res) => {
     res.render('Index', {pokemon: pokemon});
-});
+    // Pokemon.find({}, (error, allPoke) => {
+      // res.render('Index', {Pokemons: allPoke});
+    });
+// });
 
 // Show Route
 app.get("/pokemon/:id", (req, res) => {
-    // pokemon.findById(req.params.id, (error, foundPoke) => {
+    // Pokemons.findById(req.params.id, (error, foundPoke) => {
       res.render("Show", { pokemon: pokemon[req.params.id] });
     });
-//   });
+  // });
 
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+  connectToDB();
+});
